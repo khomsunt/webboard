a8730a74d6285c9290abaa721e5c6c740f0c14c9
khomsunt <khom_meanmon@hotmail.com>
2018-02-01 23:59:55 +0700

    post view add

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use \App\post;
+
 class PostController extends Controller
 {
+    public function index(){
+        $posts = post::all();
+        return view('post.index',compact('posts'));
+    }
     //
 }
