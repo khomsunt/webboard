1263ab6ae50a585ac5fc03b8c37c7fd893b3943f
khomsunt <khom_meanmon@hotmail.com>
2018-02-02 10:33:51 +0700

    add relation to post comment user

--- app/comment.php
+++ app/comment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class comment extends Model
 {
+    public function user(){
+        return $this->belongTo(User::class);
+    }
+
+    public function post(){
+        return $this->belongTo(Post::class);
+    }
+
     //
 }
--- app/post.php
+++ app/post.php
@@ -10,4 +10,8 @@ class post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
